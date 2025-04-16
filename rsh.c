d48bc826f82cc992239598bd4a75ce23828ccb2d
Jake Staples <jacob_staples@mines.edu>
2025-04-16 14:28:09 -0600

    Forgot to commit, its finished, oops

--- rsh.c
+++ rsh.c
@@ -15,7 +15,11 @@ int isAllowed(const char*cmd) {
 	// TODO
 	// return 1 if cmd is one of the allowed commands
 	// return 0 otherwise
-	
+	for (size_t i = 0; i < N; i++) {
+		if (strcmp(cmd, allowed[i]) == 0) {
+			return 1;
+		}
+	}	
 	return 0;
 }
 
@@ -25,6 +29,7 @@ int main() {
     // Add variables as needed
 
     char line[256];
+    char* argv[21];
 
     while (1) {
 
@@ -36,10 +41,69 @@ int main() {
 
 	line[strlen(line)-1]='\0';
 
-	// TODO
-	// Add code to spawn processes for the first 9 commands
-	// And add code to execute cd, exit, help commands
-	// Use the example provided in myspawn.c
+
+	// My code
+	
+	// Tokenizes the command line
+	char* tokenizedLine = strtok(line, " ");
+	if (tokenizedLine == NULL) {
+		continue;
+	}
+	// Determines if the command (first word of line) is an allowed command
+	if (!isAllowed(tokenizedLine)) {
+		printf("NOT ALLOWED!\n");
+		continue;
+	}
+
+	// Determines the command line arguments and places them in args[]
+	int argCount = 0;
+	while (tokenizedLine != NULL && argCount < 20) {
+		argv[argCount] = tokenizedLine;
+		argCount++;
+		tokenizedLine = strtok(NULL, " ");
+	}
+	// Sets end of arg list
+	argv[argCount] = NULL;
+
+	// Checks if the argument is 'exit'
+	if (strcmp(argv[0], "exit") == 0) {
+		return 0;
+	}
+
+	// Checks if the command is 'help'
+	if (strcmp(argv[0], "help") == 0) {
+		printf("The allowed commands are:\n");
+		for (size_t i = 0; i < N; i++) {
+			printf("%d: %s\n", i+1, allowed[i]);
+		}
+		continue;
+	}
+
+	// Checks if the command is 'cd'
+	if (strcmp(argv[0], "cd") == 0) {
+		// Too many arguments?
+		if (argCount > 2) {
+			printf("-rsh: cd: too many arguments\n");
+		}
+		// Chdir error?
+		if (chdir(argv[1]) != 0) {
+			perror("chdir");
+		}
+		continue;
+	}
+
+	pid_t pid;
+	int status;
+	posix_spawnattr_t attr;
+	posix_spawnattr_init(&attr);
+
+	if (posix_spawnp(&pid, argv[0], NULL, &attr, argv, environ) != 0) {
+		perror("spawn failure");
+	} else {
+		waitpid(pid, &status, 0);
+	}
+
+	posix_spawnattr_destroy(&attr);
 
     }
     return 0;
